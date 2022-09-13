452f75d883cf1e797473f5eea576300c834eea72
awalke28 <awalke28@students.towson.edu>
2022-09-13 13:28:17 -0700

    traffic manager addition

---
+++ modules/traffmgr-lb/main.tf
@@ -0,0 +1,37 @@
+resource "random_id" "server" {
+  keepers = {
+    azi_id = 1
+  }
+
+  byte_length = 8
+}
+
+resource "azurerm_resource_group" "serverrg" {
+  name     = var.rgName
+  location = var.rglocation
+}
+
+resource "azurerm_traffic_manager_profile" "profile" {
+  name                   = var.profileName
+  resource_group_name    = azurerm_resource_group.serverrg.name
+  traffic_routing_method = "Weighted"
+
+  dns_config {
+    relative_name = random_id.server.hex
+    ttl           = 100
+  }
+
+  monitor_config {
+    protocol                     = "HTTP"
+    port                         = 80
+    path                         = "/"
+    interval_in_seconds          = 30
+    timeout_in_seconds           = 9
+    tolerated_number_of_failures = 3
+  }
+
+  tags = {
+    environment = "Production"
+  }
+}
+
---
+++ modules/traffmgr-lb/variables.tf
@@ -0,0 +1,12 @@
+variable "rgName" {
+    type = string
+    description = "rg Name"
+}
+variable "rglocation" {
+    type = string
+    description = "rg location"
+}
+variable "profileName" {
+    type = string
+    description = "traffic manager profile"
+}
